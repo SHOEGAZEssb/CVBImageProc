efd3c0b4ad89316add36efc7f55b652a34a6b2e8
Tim Stadler <timstadler905@googlemail.com>
2020-04-29 10:39:43 +0200

    Add test for invert processor

---
+++ CVBImageProcTest/Processing/InvertTest.cs
@@ -0,0 +1,29 @@
+﻿using CVBImageProc.Processing;
+using NUnit.Framework;
+
+namespace CVBImageProcTest.Processing
+{
+  /// <summary>
+  /// Tests for the <see cref="Invert"/> processor.
+  /// </summary>
+  [TestFixture]
+  class InvertTest
+  {
+    [Test]
+    public void ProcessingTest()
+    {
+      // given: test image
+      using (var img = TestHelper.CreateMonoTestImage(new byte[] { 10, 50, 255, 0 }))
+      {
+        var invert = new Invert();
+
+        // when: applying the processor
+        using (var processedImage = invert.Process(img))
+        {
+          // then: pixels inverted
+          CollectionAssert.AreEqual(new byte[] { 245, 205, 0, 255 }, processedImage.GetPixels());
+        }
+      }
+    }
+  }
+}
