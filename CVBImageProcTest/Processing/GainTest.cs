475524a7ea0e7f79dcf5b1722e2482427df76f60
Tim Stadler <timstadler905@googlemail.com>
2020-04-29 10:18:45 +0200

    Add more gain tests

--- CVBImageProcTest/Processing/GainTest.cs
+++ CVBImageProcTest/Processing/GainTest.cs
@@ -9,22 +9,45 @@ namespace CVBImageProcTest.Processing
   class GainTest
   {
     [Test]
-    public void ProcessingTest()
+    public void ProcessingNoWrapAroundTest()
     {
       // given: test image
-      byte[] pixelValues = new byte[4] { 0, 10, 20, 30 };
+      byte[] pixelValues = new byte[4] { 0, 10, 20, 250 };
       using(var img = TestHelper.CreateMonoTestImage(pixelValues))
       {
         var gain = new Gain
         {
-          GainValue = 12
+          GainValue = 12,
+          WrapAround = false
         };
 
         // when: applying the processor
         using (Image processedImage = gain.Process(img))
         {
           // then: gain applied
-          CollectionAssert.AreEqual(pixelValues.Select(b => b + gain.GainValue), processedImage.GetPixels());
+          CollectionAssert.AreEqual(new byte[] { 12, 22, 32, 255 }, processedImage.GetPixels());
+        }
+      }
+    }
+
+    [Test]
+    public void ProcessingWrapAroundTest()
+    {
+      // given: test image
+      byte[] pixelValues = new byte[4] { 0, 10, 20, 250 };
+      using (var img = TestHelper.CreateMonoTestImage(pixelValues))
+      {
+        var gain = new Gain
+        {
+          GainValue = 12,
+          WrapAround = true
+        };
+
+        // when: applying the processor
+        using (Image processedImage = gain.Process(img))
+        {
+          // then: gain applied
+          CollectionAssert.AreEqual(new byte[] { 12, 22, 32, 6 }, processedImage.GetPixels());
         }
       }
     }
