5d7d5107c2bc0ca463b394ed5d4c505c676b6083
Tim Stadler <timstadler905@googlemail.com>
2020-05-01 01:43:09 +0200

    Add tests for ValueProvider

---
+++ CVBImageProcTest/Processing/ValueProvider/IntValueProviderTest.cs
@@ -0,0 +1,26 @@
+﻿using CVBImageProc.Processing.ValueProvider;
+using NUnit.Framework;
+
+namespace CVBImageProcTest.Processing.ValueProvider
+{
+  /// <summary>
+  /// Tests for the <see cref="IntValueProvider"/>.
+  /// </summary>
+  [TestFixture]
+  class IntValueProviderTest
+  {
+    [Test]
+    public void ProvideFixedValueTest()
+    {
+      // given: value provider
+      var p = new IntValueProvider(0, 255)
+      {
+        FixedValue = 42
+      };
+
+      // when: providing value
+      // then: correct value provided
+      Assert.That(p.Provide(), Is.EqualTo(42));
+    }
+  }
+}
