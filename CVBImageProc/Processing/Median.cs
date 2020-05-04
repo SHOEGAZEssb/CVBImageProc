d7c64d5a5d178dfe40489306bc610c94f0e554f3
Tim Stadler <timstadler905@googlemail.com>
2020-05-04 12:27:42 +0200

    Add base class for filter processors

--- CVBImageProc/Processing/Median.cs
+++ CVBImageProc/Processing/Median.cs
@@ -11,21 +11,21 @@ namespace CVBImageProc.Processing
   /// </summary>
   [DataContract]
   [SubProcessor]
-  public class Median : IFilter, ICanProcessIndividualPixel, IProcessIndividualPlanes, ICanProcessIndividualRegions
+  public class Median : FilterBase
   {
     #region IProcessor Implementation
 
     /// <summary>
     /// Name of the processor.
     /// </summary>
-    public string Name => "Median";
+    public override string Name => "Median";
 
     /// <summary>
     /// Processes the <paramref name="inputImage"/>.
     /// </summary>
     /// <param name="inputImage">Image to process.</param>
     /// <returns>Processed image.</returns>
-    public Image Process(Image inputImage)
+    public override Image Process(Image inputImage)
     {
       if (inputImage == null)
         throw new ArgumentNullException(nameof(inputImage));
@@ -42,53 +42,6 @@ namespace CVBImageProc.Processing
       return inputImage;
     }
 
-    #endregion IProcessorImplementation
-
-    #region ICanProcessIndividualPixel Implementation
-
-    /// <summary>
-    /// Filter chain for the processor.
-    /// </summary>
-    [DataMember]
-    public PixelFilterChain PixelFilter { get; set; } = new PixelFilterChain();
-
-    #endregion ICanProcessIndividualPixel Implementation
-
-    #region ICanProcessIndividualRegions Implementation
-
-    /// <summary>
-    /// If true, uses the <see cref="AOI"/>
-    /// while processing.
-    /// </summary>
-    [DataMember]
-    public bool UseAOI { get; set; }
-
-    /// <summary>
-    /// The AOI to process.
-    /// </summary>
-    [DataMember]
-    public Rect AOI { get; set; }
-
-    #endregion ICanProcessIndividualRegions Implementation
-
-    #region IProcessIndividualPlanes Implementation
-
-    /// <summary>
-    /// Index of the plane to invert.
-    /// </summary>
-    [DataMember]
-    public int PlaneIndex { get; set; }
-
-    #endregion IProcessIndividualPlanes Implementation
-
-    #region Properties
-
-    /// <summary>
-    /// Kernel size to use.
-    /// </summary>
-    [DataMember]
-    public KernelSize KernelSize { get; set; }
-
-    #endregion Properties
+    #endregion IProcessor Implementation
   }
 }
