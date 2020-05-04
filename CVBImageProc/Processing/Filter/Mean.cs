dd0f7c27a79e2e53f2a8005b928c344dab5a5f72
Tim Stadler <timstadler905@googlemail.com>
2020-05-04 16:02:43 +0200

    Add mean filter processor

---
+++ CVBImageProc/Processing/Filter/Mean.cs
@@ -0,0 +1,37 @@
+﻿using Stemmer.Cvb;
+using System;
+using System.Linq;
+using System.Runtime.Serialization;
+
+namespace CVBImageProc.Processing.Filter
+{
+  [DataContract]
+  class Mean : FilterBase
+  {
+    /// <summary>
+    /// Name of the processor.
+    /// </summary>
+    public override string Name => "Mean";
+
+    /// <summary>
+    /// Processes the <paramref name="inputImage"/>.
+    /// </summary>
+    /// <param name="inputImage">Image to process.</param>
+    /// <returns>Processed image.</returns>
+    public override Image Process(Image inputImage)
+    {
+      if (inputImage == null)
+        throw new ArgumentNullException(nameof(inputImage));
+
+      int kernelSize = (int)Math.Floor(KernelSize.GetKernelNumber() / 2.0);
+      var plane = ProcessingHelper.ProcessMonoKernel(inputImage.Planes[PlaneIndex], (kl) =>
+      {
+        var stripped = kl.Where(b => b.HasValue);
+        return (byte)(stripped.Sum(b => b.Value) / stripped.Count());
+      }, KernelSize, this.GetProcessingBounds(inputImage), PixelFilter);
+
+      plane.CopyTo(inputImage.Planes[PlaneIndex]);
+      return inputImage;
+    }
+  }
+}
