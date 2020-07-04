1a3ea5dbda32b194bbe2bd5b6315f4e3cd2576a0
Tim Stadler <timstadler905@googlemail.com>
2020-07-04 17:10:00 +0200

    Add EnumDescriptionConverter to display kernel sizes correctly

---
+++ CVBImageProc/MVVM/Converter/EnumDescriptionConverter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Reflection;
+using System.Windows.Data;
+
+namespace CVBImageProc.MVVM.Converter
+{
+  /// <summary>
+  /// Converter to convert the description attribute of an enum to a string.
+  /// </summary>
+  public class EnumDescriptionConverter : IValueConverter
+  {
+    /// <summary>
+    /// Gets the enum description of the given <paramref name="enumObj"/>.
+    /// </summary>
+    /// <param name="enumObj">Enum to get the description of.</param>
+    /// <returns>Description as string.</returns>
+    private static string GetEnumDescription(Enum enumObj)
+    {
+      FieldInfo fieldInfo = enumObj.GetType().GetField(enumObj.ToString());
+      object[] attribArray = fieldInfo.GetCustomAttributes(false);
+      return attribArray.Length == 0 ? enumObj.ToString() : (attribArray[0] as DescriptionAttribute).Description;
+    }
+
+    /// <summary>
+    /// Converts the given <paramref name="value"/> to its enum description.
+    /// </summary>
+    /// <param name="value">Enum to convert.</param>
+    /// <param name="targetType">Ignored.</param>
+    /// <param name="parameter">Ignored.</param>
+    /// <param name="culture">Ignored.</param>
+    /// <returns></returns>
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      if (value == null || !(value is Enum myEnum))
+        return "";
+
+      string description = GetEnumDescription(myEnum);
+      return description;
+    }
+
+    /// <summary>
+    /// Not implemented.
+    /// </summary>
+    /// <param name="value">Ignored.</param>
+    /// <param name="targetType">Ignored.</param>
+    /// <param name="parameter">Ignored.</param>
+    /// <param name="culture">Ignored.</param>
+    /// <returns>Nothing.</returns>
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ CVBImageProcTest/Converter/EnumDescriptionConverterTest.cs
@@ -0,0 +1,47 @@
+﻿using CVBImageProc.MVVM.Converter;
+using NUnit.Framework;
+
+namespace CVBImageProcTest.Converter
+{
+  /// <summary>
+  /// Tests for the <see cref="EnumDescriptionConverter"/>.
+  /// </summary>
+  [TestFixture]
+  class EnumDescriptionConverterTest
+  {
+    private const string TEST1DESCRIPTION = "Test Enum Value";
+
+    private enum TestEnum
+    {
+      [System.ComponentModel.Description(TEST1DESCRIPTION)]
+      Test1,
+      Test2
+    }
+
+    [Test]
+    public void ConvertTest()
+    {
+      // given: converter
+      var conv = new EnumDescriptionConverter();
+
+      // when: converting with attribute
+      var res = conv.Convert(TestEnum.Test1, null, null, null);
+
+      // then: correct description
+      Assert.That(res, Is.EqualTo(TEST1DESCRIPTION));
+    }
+
+    [Test]
+    public void ConvertNoAttributeTest()
+    {
+      // given: converter
+      var conv = new EnumDescriptionConverter();
+
+      // when: converting without attribute
+      var res = conv.Convert(TestEnum.Test2, null, null, null);
+
+      // then: ToString result
+      Assert.That(res, Is.EqualTo(TestEnum.Test2.ToString()));
+    }
+  }
+}
