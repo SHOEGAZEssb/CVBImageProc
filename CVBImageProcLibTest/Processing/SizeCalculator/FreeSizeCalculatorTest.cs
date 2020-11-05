de8b412beb19e9c5c110cc0f549a513df1ed9c0f
Tim Stadler <timstadler905@googlemail.com>
2020-11-05 11:26:13 +0100

    Add tests for SizeCalculators

---
+++ CVBImageProcLibTest/Processing/SizeCalculator/FreeSizeCalculatorTest.cs
@@ -0,0 +1,28 @@
+﻿using CVBImageProcLib.Processing.SizeCalculator;
+using NUnit.Framework;
+
+namespace CVBImageProcLibTest.Processing.SizeCalculator
+{
+  /// <summary>
+  /// Tests for the <see cref="FreeSizeCalculator"/>.
+  /// </summary>
+  [TestFixture]
+  class FreeSizeCalculatorTest
+  {
+    [Test]
+    public void GetCalculatedSizeTest()
+    {
+      // given: image and size calculator
+      var calc = new FreeSizeCalculator();
+      calc.Width = 64;
+      calc.Height = 64;
+
+      // when: getting size
+      var size = calc.GetCalculatedSize(null);
+
+      // then: size is correct
+      Assert.That(size.Width, Is.EqualTo(calc.Width));
+      Assert.That(size.Height, Is.EqualTo(calc.Height));
+    }
+  }
+}
---
+++ CVBImageProcLibTest/Processing/SizeCalculator/PercentageSizeCalculatorTest.cs
@@ -0,0 +1,42 @@
+﻿using CVBImageProcLib.Processing.SizeCalculator;
+using NUnit.Framework;
+using Stemmer.Cvb;
+
+namespace CVBImageProcLibTest.Processing.SizeCalculator
+{
+  /// <summary>
+  /// Tests for the <see cref="PercentageSizeCalculator"/>.
+  /// </summary>
+  [TestFixture]
+  class PercentageSizeCalculatorTest
+  {
+    [Test]
+    public void GetCalculatedSizeTest()
+    {
+      // given: image and calculator
+      using (var img = new Image(128, 128))
+      {
+        var calc = new PercentageSizeCalculator();
+        calc.Percentage = 0.5;
+
+        // when: getting size
+        var size = calc.GetCalculatedSize(img);
+
+        // then: size correct
+        Assert.That(size.Width, Is.EqualTo(img.Width * calc.Percentage));
+        Assert.That(size.Height, Is.EqualTo(img.Height * calc.Percentage));
+      }
+    }
+
+    [Test]
+    public void GetCalculatedSizeNoImageTest()
+    {
+      // given: calculator
+      var calc = new PercentageSizeCalculator();
+
+      // when: getting size with no image
+      // then: ArgumentNullException
+      Assert.That(() => calc.GetCalculatedSize(null), Throws.ArgumentNullException);
+    }
+  }
+}
